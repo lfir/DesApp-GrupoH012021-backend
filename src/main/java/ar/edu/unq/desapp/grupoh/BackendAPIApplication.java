06a1c72e781d1db7e26d4dd6f840d75eebc1f0fc
Asta86 <psljp@protonmail.com>
2021-04-09 09:31:33 -0300

    Initial commit.

---
+++ src/main/java/ar/edu/unq/desapp/grupoh/BackendAPIApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoh/BackendAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
