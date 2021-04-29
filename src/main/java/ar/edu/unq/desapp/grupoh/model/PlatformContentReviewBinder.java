bbb68ff6ed63137f13fa111d3c3fa78a971bfbf5
Asta86 <psljp@protonmail.com>
2021-04-29 20:56:09 -0300

    Add classes PlatformContentReviewBinder and PlatformContentReviewBinderTest.

---
+++ src/main/java/ar/edu/unq/desapp/grupoh/model/PlatformContentReviewBinder.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.desapp.grupoh.model;
+
+import java.util.List;
+
+import ar.edu.unq.desapp.grupoh.model.AppContent.PlatformContent;
+import ar.edu.unq.desapp.grupoh.model.Review.Review;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlatformContentReviewBinder {
+	private PlatformContent platformContent;
+	private List<Review> reviews;
+
+	public String getPlatformContentImdbId() {
+		return this.platformContent.getTitleId();
+	}
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoh/model/PlatformContentReviewBinderTest.java
@@ -0,0 +1,45 @@
+package ar.edu.unq.desapp.grupoh.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.desapp.grupoh.model.AppContent.PlatformContent;
+import ar.edu.unq.desapp.grupoh.model.Review.Review;
+
+public class PlatformContentReviewBinderTest {
+	@Test
+	void whenValidDataThenPlatformContentReviewBinderWithCorrectFieldsIsCreated() {
+		PlatformContent platformContent = mock(PlatformContent.class);
+		Review review = mock(Review.class);
+		List<Review> reviews = Arrays.asList(review);
+		
+		PlatformContentReviewBinder platformContentReviewBinder = new PlatformContentReviewBinder();
+		platformContentReviewBinder.setPlatformContent(platformContent);
+		platformContentReviewBinder.setReviews(reviews);
+		
+		assertTrue(PlatformContent.class.isInstance(platformContentReviewBinder.getPlatformContent()));
+		assertTrue(Review.class.isInstance(platformContentReviewBinder.getReviews().get(0)));
+	}
+	
+	@Test
+	void whenGetPlatformContentImdbIdIsCalledItReturnsTheImdbIdOfTheTitle() {
+		String testId = "xSj9H";
+		PlatformContent platformContent = mock(PlatformContent.class);
+		given(platformContent.getTitleId()).willReturn(testId);
+		Review review = mock(Review.class);
+		List<Review> reviews = Arrays.asList(review);
+		
+		PlatformContentReviewBinder platformContentReviewBinder = new PlatformContentReviewBinder(
+			platformContent, reviews
+		);
+		
+		assertEquals(testId, platformContentReviewBinder.getPlatformContentImdbId());
+	}
+}
