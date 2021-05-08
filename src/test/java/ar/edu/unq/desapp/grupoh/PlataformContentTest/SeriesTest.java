3c4eaf90c9129c7c1ffaaae5e700d6a24aa2114a
Asta86 <psljp@protonmail.com>
2021-05-08 16:41:47 -0300

    Add test data.

--- src/test/java/ar/edu/unq/desapp/grupoh/PlataformContentTest/SeriesTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoh/PlataformContentTest/SeriesTest.java
@@ -9,35 +9,58 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SeriesTest {
     @Test
     void whenValidDataThenSeriesWithCorrectDataIsCreated () {
+    	String titleId = "as2d2d";
+    	
         TitleInformation titleInformation = new TitleInformation("short", "The Sea", "Baignade en mer");
-        String titleId = "as2d2d";
+        titleInformation.setId(Long.valueOf("1"));
+        
         TitleDate titleDate = new TitleDate(LocalDate.parse("1985-10-10"), null);
+        titleDate.setId(Long.valueOf("3"));
+        
         ArrayList<Genre> genres = new ArrayList<>();
-        genres.add(new Genre("Historical", "Cine"));
+        Genre testGenre = new Genre();
+        testGenre.setId(Long.valueOf("12"));
+        testGenre.setName("Historical");
+        testGenre.setProduction("Cine");
+        genres.add(testGenre);
         genres.add(new Genre("Short", "Cine"));
+        
         Principal director = new Principal("Director", "Director of photography", "/N");
-        Principal writer = new Principal("Writer", "Writer", "/N");
+        Principal writer = new Principal();
+        writer.setId(Long.valueOf("1"));
+        writer.setCategory("Writer");
+        writer.setCharacters("Writer");
+        writer.setJob("/N");
         ArrayList<Principal> directors = new ArrayList<>();
         directors.add(director);
         ArrayList<Principal> writers = new ArrayList<>();
         writers.add(writer);
         Crew crew = new Crew(directors, writers);
+        crew.setId(Long.valueOf("2"));
+        
         Principal actor = new Principal("Actor", "/N", "Self");
         ArrayList<Principal> actors = new ArrayList<>();
         actors.add(actor);
 
-        Episode episode1 = new Episode(1,1);
+        Episode episode1 = new Episode();
+        episode1.setSeasonNumber(1);
+        episode1.setEpisodeNumber(1);
         Episode episode2 = new Episode(1,2);
 
         ArrayList<Episode> episodes = new ArrayList<>();
         episodes.add(episode1);
         episodes.add(episode2);
         Series series = new Series(titleId, titleInformation, false, titleDate, 54, genres, crew, actors, episodes);
-
+        Series series2 = new Series();
+        series2.setEpisodes(episodes);
+        
+        assertTrue(Series.class.isInstance(series2));
+        assertTrue(Episode.class.isInstance(series2.getEpisodes().get(0)));
         assertEquals(series.getTitleId(), titleId);
 
         // Title asserts
