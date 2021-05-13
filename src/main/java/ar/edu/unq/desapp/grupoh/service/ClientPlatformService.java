5212cf23a97f25bf8482c0e5c9c590698fa0d546
Asta86 <psljp@protonmail.com>
2021-05-13 19:26:34 -0300

    Store hashed passwords.

--- src/main/java/ar/edu/unq/desapp/grupoh/service/ClientPlatformService.java
+++ src/main/java/ar/edu/unq/desapp/grupoh/service/ClientPlatformService.java
@@ -3,6 +3,7 @@ package ar.edu.unq.desapp.grupoh.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import ar.edu.unq.desapp.grupoh.model.ClientPlatform;
@@ -15,10 +16,13 @@ public class ClientPlatformService {
 	
 	public String add(AddClientPlatformRequestBody requestBody) {
 		String apiKey = UUID.randomUUID().toString();
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(requestBody.getPassword());
 		ClientPlatform clientPlatform = new ClientPlatform(
-			requestBody.getUsername(), requestBody.getPassword(), 
+			requestBody.getUsername(), hashedPassword, 
 			apiKey, requestBody.getPlatformName()
 		);
+		
 		this.clientRepository.save(clientPlatform);
 		return apiKey;
 	}
