1aeb9e9bc7bf623ab17f2888439e88ffa0e9dce5
Asta86 <psljp@protonmail.com>
2021-05-13 13:56:42 -0300

    Check for required fields before deserializing JSON.

--- src/main/java/ar/edu/unq/desapp/grupoh/service/AddReviewRequestBody.java
+++ src/main/java/ar/edu/unq/desapp/grupoh/service/AddReviewRequestBody.java
@@ -2,21 +2,37 @@ package ar.edu.unq.desapp.grupoh.service;
 
 import java.time.LocalDate;
 
+import org.springframework.lang.NonNull;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder(builderClassName = "Builder", toBuilder = true)
+@JsonDeserialize(builder = AddReviewRequestBody.Builder.class)
 @Getter
 public class AddReviewRequestBody {
+	@JsonPOJOBuilder(withPrefix = "")
+    public static class Builder {}
+	@NonNull
 	private String description;
+	@NonNull
 	private String fullDescription;
+	@NonNull
 	private Integer rating;
 	private Boolean spoilerAlert;
+	@NonNull
 	private LocalDate date;
+	@NonNull
 	private String originPlatformName;
+	@NonNull
 	private String platformUserId;
 	private String nickname;
+	@NonNull
 	private String language;
 	private String country;
-	private Integer likeDislikeScore;
-	private String userReports;
+	@NonNull
 	private String contentImdbId;
 }
