86a52d4e590eb728cd48de307430b02e497c0c3a
Asta86 <psljp@protonmail.com>
2021-05-19 11:20:42 -0300

    Add method to get ids of Binders with Reviews with rating greater than the specified value.

--- src/main/java/ar/edu/unq/desapp/grupoh/persistence/PlatformContentReviewBinderRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupoh/persistence/PlatformContentReviewBinderRepository.java
@@ -1,5 +1,7 @@
 package ar.edu.unq.desapp.grupoh.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,6 +9,9 @@ import org.springframework.data.repository.query.Param;
 import ar.edu.unq.desapp.grupoh.model.PlatformContentReviewBinder;
 
 public interface PlatformContentReviewBinderRepository extends JpaRepository<PlatformContentReviewBinder, Long> {
-	@Query("SELECT b FROM PlatformContentReviewBinder b WHERE b.platformContent.titleId =:titleId")
+	@Query("SELECT b FROM PlatformContentReviewBinder b WHERE b.platformContent.titleId = :titleId")
 	PlatformContentReviewBinder findByContentImdbId(@Param("titleId") String titleId);
+	
+	@Query("SELECT b.id FROM PlatformContentReviewBinder b JOIN b.reviews r WHERE r.rating >= :minVal")
+	List<Long> getIdsWithReviewsOfRatingGTE(@Param("minVal") Integer minVal);
 }
