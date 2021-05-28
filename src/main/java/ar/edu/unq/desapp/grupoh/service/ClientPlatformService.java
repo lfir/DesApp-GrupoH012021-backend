b6e4e60f6e8ace834f883867344df6f881a3264d
Asta86 <psljp@protonmail.com>
2021-05-28 14:50:29 -0300

    Define PasswordEncoder as a Bean.

--- src/main/java/ar/edu/unq/desapp/grupoh/service/ClientPlatformService.java
+++ src/main/java/ar/edu/unq/desapp/grupoh/service/ClientPlatformService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +16,13 @@ import ar.edu.unq.desapp.grupoh.persistence.ClientPlatformRepository;
 public class ClientPlatformService {
 	@Autowired
 	private ClientPlatformRepository clientRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@Transactional
 	public String add(ClientPlatformDTO requestBody) {
 		String apiKey = UUID.randomUUID().toString();
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashedPassword = passwordEncoder.encode(requestBody.getPassword());
+		String hashedPassword = this.passwordEncoder.encode(requestBody.getPassword());
 		ClientPlatform clientPlatform = new ClientPlatform(
 			requestBody.getUsername(), hashedPassword, 
 			apiKey, requestBody.getPlatformName()
